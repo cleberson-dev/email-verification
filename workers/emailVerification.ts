1a74e21502118a005b034d20030b78d0a230fba3
cleberson-dev <cleberson.dev@gmail.com>
2020-07-08 15:01:12 -0300

    Ajustado worker para gerar e persistir os tokens

--- workers/emailVerification.ts
+++ workers/emailVerification.ts
@@ -1,14 +1,25 @@
 import redis from "redis";
+import { PrismaClient } from "@prisma/client";
+import cryptoRandomString from "crypto-random-string";
+
+const prisma = new PrismaClient();
 
 const worker = redis.createClient({
-  host: process.env.REDIS_HOST
+  host: process.env.REDIS_HOST,
 });
 
-worker.on('message', (channel, email) => {
-  if (channel !== 'USER_CREATED') return;
+worker.on("message", async (channel, email) => {
+  if (channel !== "USER_CREATED") return;
 
   console.log(`User created with email ${email}`);
+
+  const token = cryptoRandomString({ type: "base64", length: 32 });
+
+  await prisma.userValidationToken.create({
+    data: { user: { connect: { email } }, token },
+  });
+
+  console.log(`Token created (${token}) for ${email}`);
 });
 
-
-worker.subscribe('USER_CREATED');
+worker.subscribe("USER_CREATED");
