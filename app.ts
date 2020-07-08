aa62a6912db212de70e91f73b7e785fedd66d49f
cleberson-dev <cleberson.dev@gmail.com>
2020-07-08 11:26:32 -0300

    Criado rota para registrar usuário

--- app.ts
+++ app.ts
@@ -21,4 +21,26 @@ app.get('/users', async (req, res) => {
   res.json({ data: users });
 });
 
+app.post('/users', async (req, res) => {
+  const { email, password, name } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    return res.json({ message: "'email' e 'password' são necessários." });
+  }
+
+  try {
+    const newUser = await prisma.user.create({ 
+      data: { email, password, name },
+      select: { id: true, email: true, password: true }
+    });
+    res.status(200);
+    res.json(newUser);
+  } catch (e) {
+    res.status(400);
+    console.log(e);
+    res.json({ message: e.message });
+  }
+});
+
 export default app;
