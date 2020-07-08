7e2efd6f9ca3dae1398ee6197812e3ebabe0bf32
cleberson-dev <cleberson.dev@gmail.com>
2020-07-08 11:20:54 -0300

    Criado rota para obter todos os usuários

--- app.ts
+++ app.ts
@@ -1,10 +1,24 @@
 import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
-app.get('/', (req: Request, res: Response) => {
+app.use(express.json());
+
+app.get('/', (req, res) => {
   res.status(200);
   res.json({ "Hello": "World" });
+});
+
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany({
+    select: { id: true, email: true, name: true }
+  });
+
+  res.status(200);
+  res.json({ data: users });
 });
 
 export default app;
